ef9ed1711217cf075f646249798417783dc19aa1
JustAStudent2018 <medardo.sosobrado@elev.cybergymnasiet.se>
2021-04-13 00:10:10 +0200

    Update4.0

---
+++ app.js
@@ -0,0 +1,24 @@
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+
+tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
+tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
+tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
+tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
+tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+
+function imageGallery() {
+    const highlight = document.querySelector(".gallery-hightlight");
+    const previews = document.querySelectorAll(".room-preview img");
+  
+    previews.forEach(preview => {
+      preview.addEventListener("click", function() {
+        const smallSrc = this.src;
+        const bigSrc = smallSrc.replace("small", "big");
+        previews.forEach(preview => preview.classList.remove("room-active"));
+        highlight.src = bigSrc;
+        preview.classList.add("room-active");
+      });
+    });
+  }
+  
+  imageGallery();
---
+++ game.js
@@ -0,0 +1,6 @@
+function mobile320() {
+    document.querySelector(".browser1").setAttribute("src", "https://tfue.io/3d");
+  }
+  
+  document.getElementsByTagName("button")[0].onclick = mobile320;
+  
